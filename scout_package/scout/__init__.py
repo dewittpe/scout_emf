5f3fb394ff11d0dceda34e77d82d2c7527eac2d4
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-05-18 08:39:02 -0600

    setting up scout package structure

---
+++ scout_package/scout/__init__.py
@@ -0,0 +1,3 @@
+# __init__.py
+
+from .utilities import isfloat
---
+++ scout_package/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name = "scout",
+        version = "0.0.1",
+        author = "Peter E. DeWitt, Chioke Harris, Jared Langevin",
+        author_email = "peter.dewitt@nrel.gov, chioke.harris@nrel.gov, jarad.langevin@lbl.gov",
+        description = "Estimate impacts of Energy Conservation Measures in the U.S. residential and commercial building sectors.",
+        long_description = long_description,
+        long_description_content_type = "text/markdown",
+        url = "https://github.com/dewittpe/scout_emf",
+        packages = setuptools.find_packages(),
+        python_requires = '>=3.6',
+        )
