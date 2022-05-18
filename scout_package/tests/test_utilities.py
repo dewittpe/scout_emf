4094255a4d2dd875f9dd24f7c422c923c3952227
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-05-18 15:03:35 -0600

    extend testing for isfloat

--- scout_package/tests/test_utilities.py
+++ scout_package/tests/test_utilities.py
@@ -13,5 +13,8 @@ class TestUtilities(unittest.TestCase):
     def test_isfloat_2(self):
         self.assertFalse(scout.isfloat("a"))
 
+    def test_isfloat_3(self):
+        self.assertFalse(scout.isfloat([3.1, 1.8]))
+
 if __name__ == "__main__":
     unittest.main()
