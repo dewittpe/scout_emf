d302bfcdc7ce2943b531cb26bcb6466ea92ba2b6
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-05-24 15:26:11 -0600

    [skip ci] cost effective plots added dash-test.py

--- dash-test.py
+++ dash-test.py
@@ -7,8 +7,45 @@ from dash.dependencies import Input, Output
 from dash import dcc
 from dash import html
 
+def unique_strings(l):
+    list_set = set(l)
+    ul = (list(list_set))
+    return '; '.join(ul)
+
+
+# import data
 results_1 = scout.ecm_results(path = "./Results_Files_3/ecm_results_1-1.json.gz")
+
+# build useful things for ui
 ecms = [{"label" : l, "value" : l} for l in set(results_1.financial_metrics.ecm)]
+years = [y for y in set(results_1.mas_by_category.year)]
+years.sort()
+
+# plotting data sets
+ces_plot_data =\
+    results_1.mas_by_category\
+            .groupby(["scenario", "ecm", "metric", "year"])\
+            .agg({
+                "value" : "sum",
+                "building_class" : unique_strings,
+                "region" : unique_strings,
+                "end_use" : unique_strings
+                })\
+            .reset_index()
+ces_plot_data = \
+        pd.pivot_table(ces_plot_data,
+                values = "value",
+                index = ["scenario", "ecm", "building_class", "end_use", "year"],
+                columns = ["metric"]
+                )\
+        .reset_index()\
+        .merge(results_1.financial_metrics,
+                how = "left",
+                on = ["ecm", "year"])
+
+ces_plot_data
+
+        
 
 app = dash.Dash(external_stylesheets = [dbc.themes.BOOTSTRAP])
 
@@ -61,7 +98,8 @@ fm = html.Div([
             options = [
                 {"label" : "Aggregated by Year", "value" : "agg_year"},
                 {"label" : "All ECMS", "value" : "all_ecms"},
-                {"label" : "Select an ECM:", "value" : "each_ecm"}],
+                {"label" : "Select an ECM:", "value" : "each_ecm"}
+                ],
             value = "agg_year",
             clearable = False
             )],
@@ -71,6 +109,24 @@ fm = html.Div([
     html.Div(id = "fm-output-container", style = {'width' : '90%', 'height': '900px'})
 ])
 
+ces = html.Div([
+    html.H1("Cost Effective Savings"),
+    html.Div([
+        dcc.Dropdown(id = "ces_cce_dropdown",
+            options = [
+                {"label" : "Avoided CO\u2082 Emissions (MMTons)", "value" : "carbon"},
+                {"label" : "Energy Cost Savings (USD)",           "value" : "cost"},
+                {"label" : "Energy Savings (MMBtu)",              "value" : "energy"}
+                ],
+            value = "carbon",
+            clearable = False
+            )],
+        style = {"width" : "25%", "display" : "inline-block"}
+        ),
+    html.Div([dcc.Dropdown(id = "year_dropdown", options = years, value = years[0], clearable = False)], id = "ecm_dropdown_div", style = {"min-width" : "500px", "display" : "inline-block"}),
+    html.Div(id = "ces-output-container", style = {'width' : '90%', 'height': '900px'})
+])
+
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def render_page_content(pathname):
     if pathname == "/":
@@ -78,7 +134,7 @@ def render_page_content(pathname):
     elif pathname == "/fm":
         return fm
     elif pathname == "/ces":
-        return html.P("Cost Effective Savings")
+        return ces
     # If the user tries to reach a different page, return a 404 message
     return dbc.Jumbotron(
         [
@@ -103,7 +159,7 @@ def show_hide_ecm_dropdown(value):
         Input('fm_dropdown', 'value'),
         Input('ecm_dropdown', 'value')
         )
-def update_output(fm_dropdown_value, ecm_dropdown_value):
+def update_fm_output(fm_dropdown_value, ecm_dropdown_value):
     if fm_dropdown_value == "agg_year":
         fig = px.line(
                 data_frame = results_1.financial_metrics\
@@ -154,8 +210,46 @@ def update_output(fm_dropdown_value, ecm_dropdown_value):
             ])
         return fm_each_ecm
     else:
-        return f"impressive, everything you did is wrong"
+        return "impressive, everything you did is wrong"
 
+
+@app.callback(
+        Output('ces-output-container', 'children'),
+        Input('ces_cce_dropdown', 'value'),
+        Input('year_dropdown', 'value')
+        )
+def update_ces_output(ces_dropdown_value, year_dropdown_value):
+    if ces_dropdown_value == "carbon":
+        m = "Avoided CO\u2082 Emissions (MMTons)"
+    elif ces_dropdown_value == "cost":
+        m = "Energy Cost Savings (USD)"
+    elif ces_dropdown_value == "energy":
+        m = "Energy Savings (MMBtu)"
+    else: 
+        m = None
+
+    fig = px.scatter(
+                ces_plot_data[(ces_plot_data.year == year_dropdown_value)]
+            , x = m
+            , y = "value"
+            , symbol = "building_class"
+            , color = "end_use"
+            , facet_col = "scenario"
+            , facet_row = "metric"
+            , hover_data = {
+                "ecm": True,
+                m : True,
+                "value": True,
+                "end_use": True,
+                "building_class": True
+                }
+            )
+    fig.update_yaxes(title_text = "", matches = None, exponentformat = "e")
+    fig.update_xaxes(exponentformat = "e")
+    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
+    fig.for_each_annotation(lambda a: a.update(text = a.text.replace(" (", "<br>(")))
+    fig.update_layout(autosize = False, width = 1200, height = 800)
+    return dcc.Graph(figure = fig)
 
 
 
