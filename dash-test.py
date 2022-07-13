76089ae7723f9e85fc9a973090330de2d5182dcf
Peter DeWitt <peter.dewitt@nrel.gov>
2022-07-13 13:19:21 -0600

    remove deprecated Jumbotron

--- dash-test.py
+++ dash-test.py
@@ -201,11 +201,11 @@ def render_page_content(pathname):
     elif pathname == "/cms_v_ums":
         return cms_v_ums()
     # If the user tries to reach a different page, return a 404 message
-    return dbc.Jumbotron(
+    return dbc.Div(
         [
             html.H1("404: Not found", className="text-danger"),
             html.Hr(),
-            html.P(f"The pathname {pathname} was not recognised."),
+            html.P(f"The pathname {pathname} was not recognised.")
         ]
     )
 
