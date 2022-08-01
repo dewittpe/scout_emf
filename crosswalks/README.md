0cad2281443c8b6b408d620b9d038afcca009295
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-08-01 10:23:41 -0600

    add files for building up a crosswalk

---
+++ crosswalks/README.md
@@ -0,0 +1,21 @@
+# Crosswalks
+
+There are sevaral files in this directory used to define link tables.  These are
+needed to translate from the energy conservation measures (ECM) files and the
+energy modeling forum (EMF) definitions.
+
+## Goals
+Build as many tables as needed.  Each table should have as few columns as needed
+to define a linkage.
+
+## Link tables
+
+### Regions
+
+* Definition file: regions.py
+* Output file: regions.csv
+
+*
+
+
+
---
+++ crosswalks/regions.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import itertools
+
+aia_climate_zones = ["AIA_CZ1", "AIA_CZ2", "AIA_CZ3", "AIA_CZ4", "AIA_CZ5"]
+emm = ["TRE", "FRCC", "MISW", "MISC", "MISE", "MISS", "ISNE", "NYCW", "NYUP", "PJME", "PJMW", "PJMC", "PJMD", "SRCA", "SRSE", "SRCE", "SPPS", "SPPC", "SPPN", "SRSG", "CANO", "CASO", "NWPP", "RMRG", "BASN"]
+states = ["AL", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+
+x = list(
+    itertools.chain(
+        [("AIA", r) for r in aia_climate_zones],
+        [("EMM", r) for r in emm],
+        [("States", r) for r in states]
+        )
+    )
+
+scout_regions = pd.DataFrame(data = x, columns = ["region_set", "region"])
+
+scout_regions.to_csv('regions.csv')
+
